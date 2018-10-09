018fa515de37efbfd2d25581e0ee644d045de669
Alex <xzolotarev@yandex.ru>
2018-10-09 03:58:28 +0300

    Lesson3 #2To create a program that reverses the input string (Read from right to left).

--- src/main/java/ru/zolotarev/algorithms_structures/Application.java
+++ src/main/java/ru/zolotarev/algorithms_structures/Application.java
@@ -1,23 +1,10 @@
 package main.java.ru.zolotarev.algorithms_structures;
 
 public class Application {
-
     public static void main(String[] args) {
-        Array array = new Array(3, 5, 2, 8, 4, 2, 6, 3, 4, 7, 1, 8, 5, 9, 3, 1);
-        Array array1 = new Array(3, 2, 6, 4, 7, 9, 1, 4);
-        array.getInfo();
-        System.out.println("-------------");
-        array.delete();
-        array.getInfo();
-        System.out.println("-------------");
-        array.delete(1);
-        array.getInfo();
-        System.out.println("-------------");
-        array.deleteValue(2);
-        array.getInfo();
-        System.out.println("-------------");
-        array1.sortCounting();
-        array1.getInfo();
-        System.out.println("133344567889");
+//        Stack stack = new Stack(10);
+//        stack.insert("Hello word");
+//        stack.info();
+
     }
 }
