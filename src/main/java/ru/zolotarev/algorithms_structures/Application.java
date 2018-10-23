43fe4bc2b51ccc5c196a2c4958c3f943bf1dc8a0
Alex <xzolotarev@yandex.ru>
2018-10-23 03:00:43 +0300

    Lesson5 Tree

--- src/main/java/ru/zolotarev/algorithms_structures/Application.java
+++ src/main/java/ru/zolotarev/algorithms_structures/Application.java
@@ -1,19 +1,28 @@
 package main.java.ru.zolotarev.algorithms_structures;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-//        System.out.println(recPower(2, 9));
-        TowersofHanoi towersofHanoi = new TowersofHanoi();
-        towersofHanoi.moveTower(3, 1,2,3);
+        final int TREES = 20;
+        int balanced = 0;
+        for (int i = 0; i < TREES; i++) {
+            ArrayList<Integer> a = new ArrayList<>();
+            randomNumber(a, 100);
+            Tree tree = new Tree(a);
+            balanced += (tree.isBalanced(false)) ? 1 : 0;
+        }
+        System.out.println(balanced * 100f / TREES + "%");
     }
 
-    private static int recPower(int base, int sign) {
-        if (sign == 0) return 1;
-        if (sign % 2 == 0){
-            return recPower(base * base, sign/2);
-        }
-        else {
-            return base * recPower(base, --sign);
+    public static void randomNumber(ArrayList<Integer> a, int count){
+        Random r = new Random();
+        while (a.size() < count){
+            int num = r.nextInt();
+            if (!a.contains(num)){
+                a.add(num);
+            }
         }
     }
 }
